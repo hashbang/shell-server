7ec490146878211dd41d3939d4d0455eb8a7497f
Titus Soporan <titus@tsoporan.com>
2014-09-18 18:07:31 -0400

    new configs for chat

---
+++ etc/nginx/sites-available/chat.hashbang.sh
@@ -0,0 +1,11 @@
+server {
+    listen 80;
+    server_name chat.hashbang.sh;
+    location / {
+        proxy_redirect off;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_pass http://127.0.0.1:3000;
+    }
+}
