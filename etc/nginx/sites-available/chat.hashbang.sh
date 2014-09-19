bc356889b2798ef75f9d3c21e5c5f04b61df4569
Titus Soporan <titus@tsoporan.com>
2014-09-19 11:26:47 -0400

    websocket config for chat.hashbang.sh

--- etc/nginx/sites-available/chat.hashbang.sh
+++ etc/nginx/sites-available/chat.hashbang.sh
@@ -2,10 +2,10 @@ server {
     listen 80;
     server_name chat.hashbang.sh;
     location / {
-        proxy_redirect off;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_pass http://127.0.0.1:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
     }
 }
+
