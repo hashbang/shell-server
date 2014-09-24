305291ee06d3389e45b67d3697a51d2b9260bd57
Lance Vick <lance@lrvick.net>
2014-09-24 16:44:51 -0400

    update for ssl certs

--- etc/nginx/sites-available/hashbang.sh
+++ etc/nginx/sites-available/hashbang.sh
@@ -1,6 +1,17 @@
 server {
     listen 80;
+    listen 443;
+    ssl    on;
+    ssl_certificate    /etc/nginx/ssl/server-bundle.crt;
+    ssl_certificate_key    /etc/nginx/ssl/server.key;
+
     server_name hashbang.sh;
     root /var/www/html/hashbang.sh/dist;
     index index.html;
 }
+
+#server {
+#    listen         80;
+#    server_name    hashbang.sh;
+#    rewrite        ^ https://$server_name$request_uri? permanent;
+#}
